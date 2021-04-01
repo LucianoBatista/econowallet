25c8d7a330dfea093fa1b73d57a96aeb2aa3bdde
Luciano Batista <luciano.eq@outlook.com>
2021-03-31 21:45:04 -0300

    Initial setup with basic configurations.

---
+++ project/app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Depends
+
+from project.app.config import Settings, get_settings
+
+app = FastAPI()
+
+
+@app.get("/ping")
+async def ping(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong!",
+        "environment": settings.environment,
+        "testing": settings.testing
+    }
